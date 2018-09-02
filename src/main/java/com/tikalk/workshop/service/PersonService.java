2ebd276efadc5ddf7ab4ee6765e2b2437a0a22cd
sigals <sigals@tikalk.com>
2018-09-02 22:46:15 +0300

    Co

--- src/main/java/com/tikalk/workshop/service/PersonService.java
+++ src/main/java/com/tikalk/workshop/service/PersonService.java
@@ -2,17 +2,15 @@ package com.tikalk.workshop.service;
 
 import com.tikalk.workshop.entity.Person;
 import com.tikalk.workshop.repository.PersonRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 /**
+ * Reactive service
  * Created by sigals on 10/03/2018.
  */
 @Service
