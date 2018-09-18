d81aba337e73678bf8de36cd15e029a107faf30f
sigals <sigals@tikalk.com>
2018-09-18 20:47:20 +0300

    Refactor the controller Add getAll

--- src/main/java/com/tikalk/workshop/controller/PersonController.java
+++ src/main/java/com/tikalk/workshop/controller/PersonController.java
@@ -6,10 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,18 +16,28 @@ import java.util.List;
  * Created by sigals on 03/03/2018.
  */
 @RestController
-@RequestMapping(value = "/person", produces = "application/json")
+@RequestMapping(value = "/person", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
 public class PersonController {
 
     private final static Logger LOG = LoggerFactory.getLogger(PersonController.class);
 
-    @Autowired
-    private PersonService personService;
+    private final PersonService personService;
 
-    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-    public void postPersons(@Valid @RequestBody List<Person> persons) {
+    @Autowired
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping
+    public Flux<Person> postPersons(@Valid @RequestBody List<Person> persons) {
         LOG.info("Received request");
-        personService.storePersons(persons);
+        Flux<Person> created = personService.storePersonsFlux(persons);
         LOG.info("Request processed");
+        return created;
+    }
+
+    @GetMapping
+    public Flux<Person> getAll() {
+        return personService.getAll();
     }
 }
