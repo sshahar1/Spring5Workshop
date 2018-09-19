69d9195bc38f1b60c01e76ae976f5d06794f84ea
sigals <sigals@tikalk.com>
2018-09-19 20:45:10 +0300

    Clients

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Spring5Workshop
+
+1. How may we write a test to send a flux of objects?
+2. What about unit tests for the rest controller? WebTestClient!
---
+++ src/test/java/com/tikalk/workshop/client/ControllerClient.java
@@ -0,0 +1,44 @@
+package com.tikalk.workshop.client;
+
+import com.tikalk.workshop.entity.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.Random;
+
+/**
+ * Reactive REST controller client
+ * Created by sigals on 19/09/2018.
+ */
+public class ControllerClient {
+
+    private static final Random random = new Random();
+
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerClient.class);
+
+    public static void main(String[] args) throws URISyntaxException {
+        WebClient webClient = WebClient.create("http://localhost:8080/person");
+
+        LOG.info("Sending request");
+
+        webClient
+                .post()
+                .body(BodyInserters.fromPublisher(getPayload(), Person.class))
+                .exchange()
+                .doOnNext( p -> LOG.info("Received: {}", p.statusCode()))
+                .block();
+        LOG.info("Received response");
+    }
+
+    private static Flux<Person> getPayload() {
+        return Flux.just(new Person("first " + random.nextInt(1000), "last " + random.nextInt(1000)))
+                .zipWith(Flux.interval(Duration.ofSeconds(10)))
+                .map(Tuple2::getT1);
+    }
+}
